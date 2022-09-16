436755c2198f77b64fa7bb8dcf520bb154a6cc08
CollinNatterstad <collin.ragnarokarchery@gmail.com>
2022-09-16 14:13:15 -0700

    first commit

---
+++ OHDC_1_band_name_project.py
@@ -0,0 +1,6 @@
+#objective: Create a simple program that stores user inputs and suggests a band name based on those inputs
+
+print("Hello and welcome to the band name generator!\nPlease answer the following questions and we can get started!")
+city = str(input("What City did you grow up in?\n"))
+pet = str(input("What is your favorite pet's name?\n"))
+print("Your suggested band Name: " + city +" "+pet) 
